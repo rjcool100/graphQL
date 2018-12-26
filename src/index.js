1261cd5c8065266ee3727f2457c5241ac5c54f65
rojo <rohit.joshi@i2e1.com>
2018-12-26 14:41:01 +0530

    paramtereizeed calls and returning arrays and object arrays with filteers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import {GraphQLServer} from 'graphql-yoga'
 
+const posts=[{
+    id:'1',
+    name:'post1',
+    topic:'topic1'   
+},
+{
+    id:'2',
+    name:'post2',
+    topic:'topic2'   
+},
+{
+    id:'3',
+    name:'post3',
+    topic:'topic3'   
+}]
+
 const typeDefs=`
     type Query{
-        me:User!
+        greeting(name:String!):String!,
+        arr:[Int!]!,
+        add(numbers:[Float!]):Int!
+        me:User!,
+        post:Post!,
+        posts(query:String!):[Post!]!
     }
 
     type User{
@@ -12,10 +33,51 @@ const typeDefs=`
         employed:Boolean!,
         gpa:Float
     }
+
+    type Post{
+        id:ID!,
+        name:String!,
+        category:String!,
+        date:String!
+    }
 `
 
 const resolvers={
     Query:{
+        greeting(parent,args,ctx,info){
+            if(args.name){
+                return `hello ${args.name}`
+            }
+            else{
+                return 'hello'
+            }
+        },
+        arr(){
+            return [1,2,3]
+        },
+        posts(parent,args,ctx,info){
+            if(!args.query)
+                return posts
+            else{
+                return posts.filter((post)=>{
+                    var namematch=post.name.toLowerCase().includes(args.query.toLowerCase())
+                    var topicmatch=post.topic.toLowerCase().includes(args.query.toLowerCase())
+                    return namematch||topicmatch
+                })
+            }
+        },
+        add(parent,args,ctx,info){
+            if(args.numbers.length===0)
+            {
+                return 0;
+            }
+            else{
+                return args.numbers.reduce((sum,item)=>{
+                    return sum+item
+                })
+            }
+
+        },
          me(){
             return {
                 id:'123',
@@ -23,6 +85,14 @@ const resolvers={
                 age:27,
                 employed:false,
                 gpa:null
+            }
+        },
+        post(){
+            return{
+                id:'234',
+                name:'How to GraphQL',
+                category:'tech',
+                date:'today'
             }
         }
     }
