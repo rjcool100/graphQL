c75f118d8421b89ab88bbb7207cf4dc6aa023055
rjcool100 <rohitj299@gmail.com>
2018-12-28 16:48:59 +0530

    updating data

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -13,7 +13,6 @@ const Mutation={
             ...args.data
         }
         db.users.push(user)
-
         return user
 
     },
@@ -25,7 +24,6 @@ const Mutation={
             throw new Error('the user does not exist')
         }
         var user= db.users.splice(userIndex,1)
-
         db.posts= db.posts.filter((post)=>{
             var match=post.author===args.id
 
@@ -36,39 +34,43 @@ const Mutation={
             }
             return !match
         })
-
         db.comments= db.comments.filter((comment)=>{
             return comment.author!==args.id
         })
-
         return user[0]
 
     },
-    deletePost(parent,args,{db},info){
-        const postIndex= db.users.findIndex((post)=>{
-            return post.id===args.id
+    updateUser(parent,args,{db},info){
+        const {id,data}=args
+        const user= db.users.find((user)=>{
+            return user.id===id
         })
-        if(postIndex==-1){
-            throw new Error('the post does not exist')
-        }
-        var post= db.posts.splice(postIndex,1)
+        if(user){
+            if(typeof(data.email)==='string'){
+                const emailTaken=db.users.some((user)=>{
+                    return user.email===data.email
+                })
+                if(emailTaken){
+                    throw new Error('the email is already taken')
+                }
 
-       
-        db.comments= db.comments.filter((comment)=>{
-            return comment.post!==args.id
-        })
-        return post[0]
+                user.email=data.email
+            }
 
-    },
-    deleteComment(parent,args,{db},info){
-        const commentIndex= db.users.findIndex((comment)=>{
-            return comment.id===args.id
-        })
-        if(commentIndex==-1){
-            throw new Error('the comment does not exist')
+            if(typeof(data.name)==='string'){
+                user.name=data.name
+            }
+
+            if(typeof(data.age)!=='undefined'){
+                user.age=data.age
+            }
+
+            return user
+
         }
-        var comment= db.comments.splice(commentIndex,1)
-        return comment[0]
+        else{
+            throw new Error('the user does not exist')
+        }
     },
     createPost(parent,args,{db},info){
         const userExists= db.users.some((user)=>{
@@ -85,6 +87,40 @@ const Mutation={
 
         return post
 
+    },
+    deletePost(parent,args,{db},info){
+        const postIndex= db.users.findIndex((post)=>{
+            return post.id===args.id
+        })
+        if(postIndex==-1){
+            throw new Error('the post does not exist')
+        }
+        var post= db.posts.splice(postIndex,1)
+        db.comments= db.comments.filter((comment)=>{
+            return comment.post!==args.id
+        })
+        return post[0]
+
+    },
+    updatePost(parent,args,{db},info){
+        const {id,data}=args
+        const post= db.posts.find((post)=>{
+            return post.id===id
+        })
+        if(post){
+            if(typeof(data.name)==='string'){
+                post.name=data.name
+            }
+
+            if(typeof(data.topic)==='string'){
+                post.topic=data.topic
+            }
+
+            return post
+        }
+        else{
+            throw new Error('post not found')
+        }
     },
     createComment(parent,args,{db},info){
         const userExists= db.users.some((user)=>{
@@ -104,9 +140,32 @@ const Mutation={
             ...args.data
         }
         db.comments.push(comment)
-
         return comment
-
+    },
+    deleteComment(parent,args,{db},info){
+        const commentIndex= db.users.findIndex((comment)=>{
+            return comment.id===args.id
+        })
+        if(commentIndex==-1){
+            throw new Error('the comment does not exist')
+        }
+        var comment= db.comments.splice(commentIndex,1)
+        return comment[0]
+    },
+    updateComment(parent,args,{db},info){
+        const {id,data}=args
+        const comment= db.comments.find((comment)=>{
+            return comment.id===id
+        })
+        if(comment){
+            if(typeof(data.text)==='string'){
+                comment.text=data.text
+            }
+            return comment
+        }
+        else{
+            throw new Error('comment not found')
+        }
     }
 }
 
